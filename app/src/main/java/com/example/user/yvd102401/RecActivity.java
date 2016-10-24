888e4669b6a0f04ec5f68914d81d494951bc78a0
Shaojung <shaojung@gmail.com>
2016-10-24 09:06:01 +0800

    收資料完成

--- app/src/main/java/com/example/user/yvd102401/RecActivity.java
+++ app/src/main/java/com/example/user/yvd102401/RecActivity.java
@@ -1,7 +1,9 @@
 package com.example.user.yvd102401;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class RecActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class RecActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rec);
+        TextView tv = (TextView) findViewById(R.id.textView2);
+        Intent it = getIntent();
+        String data = it.getStringExtra(Intent.EXTRA_TEXT);
+        tv.setText(data);
     }
 }
